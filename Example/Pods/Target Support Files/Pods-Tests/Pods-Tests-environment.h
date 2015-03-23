234f6f0edc734f8bf603502aea809cebdd5117c3
Adam Iredale <adam@bionicmonocle.com>
2015-03-23 15:12:58 +1100

    Liquid commits - don't look

---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 3
+#define COCOAPODS_VERSION_PATCH_Expecta 2
+
+// Patch
+#define COCOAPODS_POD_AVAILABLE_Patch
+#define COCOAPODS_VERSION_MAJOR_Patch 0
+#define COCOAPODS_VERSION_MINOR_Patch 1
+#define COCOAPODS_VERSION_PATCH_Patch 0
+
+// Specta
+#define COCOAPODS_POD_AVAILABLE_Specta
+#define COCOAPODS_VERSION_MAJOR_Specta 0
+#define COCOAPODS_VERSION_MINOR_Specta 4
+#define COCOAPODS_VERSION_PATCH_Specta 0
+
